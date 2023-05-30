729a27c0ecae986540cd2b1f09da10a28fab2940
ikrzywda <krzywdaigor890@gmail.com>
2023-05-30 16:04:19 +0200

    feature/basic-setup

---
+++ web-editor/src/routes/editor/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	let sourceCode = 'output("Hello")';
+</script>
+
+<div class="editor-stack">
+    <div style="editor">
+        <input bind:value={sourceCode}>
+    </div>
+    <div style="output">
+        <pre>{sourceCode}</pre>
+    </div>
+</div>

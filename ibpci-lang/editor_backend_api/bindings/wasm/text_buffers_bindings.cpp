44a6bfaf22cbc731ae931464070f509c13151ed9
ikrzywda <krzywdaigor890@gmail.com>
2023-06-05 13:32:19 +0200

    bindings for api

---
+++ ibpci-lang/editor_backend_api/bindings/wasm/text_buffers_bindings.cpp
@@ -0,0 +1,21 @@
+#include <emscripten/bind.h>
+#include <memory>
+#include <string>
+#include <vector>
+#include <iostream>
+#include "../../include/text_buffers.hpp"
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(StringVector) {
+  register_vector<std::string>("StringVector");
+}
+
+EMSCRIPTEN_BINDINGS(TextBuffersWrapper) {
+  class_<TextBuffers>("TextBuffers") 
+    .constructor()
+    .function("insertNewToken", &TextBuffers::insert_new_token)
+    .function("deleteToken", &TextBuffers::delete_token)
+    .function("updateTextBuffer", &TextBuffers::update_text_buffer)
+    .function("getSuggestions", &TextBuffers::get_suggestions);
+}

13947fe94ffc070354bdf357e682f8b40faaca87
ikrzywda <krzywdaigor890@gmail.com>
2023-06-13 17:45:10 +0200

    working autocomplete

--- ibpci-lang/editor_backend_api/src/autocomplete.cpp
+++ ibpci-lang/editor_backend_api/src/autocomplete.cpp
@@ -2,20 +2,21 @@
 
 namespace Autocomplete {
 
-bool get_suggestions(Trie::Node *root, std::string prefix, std::vector<std::string> &completion_list) {
+bool get_suggestions(Trie::Node* root, std::string prefix, std::vector<std::string>& completion_list) {
   if (root == nullptr) {
     return false;
   }
 
-  std::string current_token = prefix;
-  for (auto const &node : root->children) {
-    current_token.push_back(node.first);
-    if (node.second->is_end) {
-      completion_list.push_back(current_token);
-    }
-    get_suggestions(node.second, current_token, completion_list);
+  if (root->is_end) {
+    completion_list.push_back(prefix);  // Add the complete word to the completion list
+  }
+
+  for (const auto& node : root->children) {
+    std::string new_prefix = prefix + node.first;  // Update the prefix for the current child node
+    get_suggestions(node.second, new_prefix, completion_list);
   }
   return true;
 }
+
 
 }
